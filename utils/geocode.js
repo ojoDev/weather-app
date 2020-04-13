13fb8080ddab02cf9e7238bfe53b80d821f417a3
David Muñoz <dmunoz@paradigmadigital.com>
2020-04-13 16:54:30 +0200

    ES6 features

--- utils/geocode.js
+++ utils/geocode.js
@@ -1,17 +1,17 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const urlMapBox = 'http://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) +'.json?access_token=pk.eyJ1Ijoib2pvZGV2IiwiYSI6ImNrOHd2OGZ6MDAwY3IzaG54Z3V3bjloZjAifQ.Qclify8gKIKUobsVNtWhuw'
-    request( {url: urlMapBox, json: true}, (error, response) => {
+    const url = 'http://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) +'.json?access_token=pk.eyJ1Ijoib2pvZGV2IiwiYSI6ImNrOHd2OGZ6MDAwY3IzaG54Z3V3bjloZjAifQ.Qclify8gKIKUobsVNtWhuw'
+    request( {url, json: true}, (error, {body}) => {
         if (error) {
             callback('Unable to connect to geomap service') // Second argument (data) is undefined
-        } else if (response.body.features === undefined || response.body.features.length === 0) {
+        } else if (body.features === undefined || body.features.length === 0) {
             callback('Unable to find location') // Second argument (data) is undefined
         } else {
             callback(undefined, {
-                latitude: response.body.features[0].center[1],
-                longitude: response.body.features[0].center[0],
-                placename: response.body.features[0].place_name
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                placename: body.features[0].place_name
             })
         }
     })
