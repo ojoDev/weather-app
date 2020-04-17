8cac51743788456bb7732a658563cf410b71b62d
David Muñoz <dmunoz@paradigmadigital.com>
2020-04-17 15:38:11 +0200

    geocode change

--- utils/geocode.js
+++ utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'http://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) +'.json?access_token=pk.eyJ1Ijoib2pvZGV2IiwiYSI6ImNrOHd2OGZ6MDAwY3IzaG54Z3V3bjloZjAifQ.Qclify8gKIKUobsVNtWhuw'
+    const url = 'http://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) +'.json?access_token=pk.eyJ1Ijoib2pvZGV2IiwiYSI6ImNrOHd2OGZ6MDAwY3IzaG54Z3V3bjloZjAifQ.Qclify8gKIKUobsVNtWhuw&limit=1'
     request( {url, json: true}, (error, {body}) => {
         if (error) {
             callback('Unable to connect to geomap service') // Second argument (data) is undefined
